c85fe13814f0469c98d13c2ca2fc09c3456213b0
Robert Gazizov <43959043+RobertGaz@users.noreply.github.com>
2021-11-07 23:11:40 +0300

    Add files via upload

---
+++ src/data/первичная_предобработка_csv.py
@@ -0,0 +1,85 @@
+# 2016
+
+data1 = pd.read_csv('Divvy_Trips_2016_Q1.csv')
+data2_4 = pd.read_csv('Divvy_Trips_2016_04.csv')
+data2_5 = pd.read_csv('Divvy_Trips_2016_05.csv')
+data2_6 = pd.read_csv('Divvy_Trips_2016_06.csv')
+data3 = pd.read_csv('Divvy_Trips_2016_Q3.csv')
+data4 = pd.read_csv('Divvy_Trips_2016_Q4.csv')
+
+data2016 = pd.concat([data1, data2_4, data2_5, data2_6, data3, data4], ignore_index=True, copy=False)
+
+data2016.rename(columns={'starttime':'start_time', "stoptime": "end_time"}, inplace = True)
+
+repl = lambda s: s.group() + ':00' 
+data2016.start_time = data2016.start_time.str.replace(r'\s\d{1,2}:\d\d$', repl)
+data2016['time'] = pd.to_datetime(data2016.start_time, format= '%m/%d/%Y %H:%M:%S')
+
+with open('2016_.csv', mode='w', encoding='utf-8') as f_csv:
+    data2016.to_csv(f_csv, index=False)
+
+
+
+# 2017
+
+data1 = pd.read_csv('Divvy_Trips_2017_Q1.csv')
+data2 = pd.read_csv('Divvy_Trips_2017_Q2.csv')
+data3 = pd.read_csv('Divvy_Trips_2017_Q3.csv')
+data4 = pd.read_csv('Divvy_Trips_2017_Q4.csv')
+
+data2017 = pd.concat([data1, data2, data3, data4], ignore_index=True, copy=False)
+
+repl = lambda s: s.group() + ':00' 
+data2017.start_time = data2017.start_time.str.replace(r'\s\d{1,2}:\d\d$', repl)
+data2017['time'] = pd.to_datetime(data2017.start_time, format= '%m/%d/%Y %H:%M:%S')
+
+with open('2017.csv', mode='w', encoding='utf-8') as f_csv:
+    data2017.to_csv(f_csv, index=False)
+
+
+
+
+
+
+# 2018
+
+data1 = pd.read_csv('Divvy_Trips_2018_Q1.csv')
+data2 = pd.read_csv('Divvy_Trips_2018_Q2.csv')
+data3 = pd.read_csv('Divvy_Trips_2018_Q3.csv')
+data4 = pd.read_csv('Divvy_Trips_2018_Q4.csv')
+
+# здесь колонки названы по разному
+data1.columns = data2.columns
+
+data2018 = pd.concat([data1, data2, data3, data4], ignore_index=True, copy=False)
+
+# Хранится в текстовом формате с запятыми и точками 
+data2018.tripduration = data2018.tripduration.str.slice(stop=-2).str.replace(',', '').astype('int64') 
+
+data2018['time'] = pd.to_datetime(data2018.start_time, format= '%Y/%m/%d %H:%M:%S')
+
+with open('2018.csv', mode='w', encoding='utf-8') as f_csv:
+    data2018.to_csv(f_csv, index=False)
+
+
+
+
+
+
+
+# 2019
+
+data1 = pd.read_csv('Divvy_Trips_2019_Q1.csv')
+data2 = pd.read_csv('Divvy_Trips_2019_Q2.csv')
+data3 = pd.read_csv('Divvy_Trips_2019_Q3.csv')
+
+data2.columns = data1.columns
+
+data2019 = pd.concat([data1, data2, data3], ignore_index=True, copy=False)
+
+data2019.tripduration = data2019.tripduration.str.slice(stop=-2).str.replace(',', '').astype('int64') 
+
+data2019['time'] = pd.to_datetime(data2019.start_time, format= '%Y/%m/%d %H:%M:%S')
+
+with open('2019.csv', mode='w', encoding='utf-8') as f_csv:
+    data2019.to_csv(f_csv, index=False)
---
+++ src/data/построение_датасета.py
@@ -0,0 +1,57 @@
+data2016 = pd.read_csv('2016_.csv')
+data2017 = pd.read_csv('2017_.csv')
+data2018 = pd.read_csv('2018_.csv')
+data2019 = pd.read_csv('2019_.csv')
+
+df = pd.concat([data2016, data2017, data2018, data2019], ignore_index=True, copy=False)
+df.drop_duplicates(subset='trip_id', inplace=True)
+df.drop(columns = ['usertype', 'start_time', 'end_time', 'trip_id', 'bikeid', 'from_station_id', 'from_station_name', 'to_station_id', 'to_station_name'], inplace=True)
+df.dropna(subset=['gender', 'birthyear'], inplace=True)
+
+df.gender = df.gender.astype('category')
+df.birthyear = df.birthyear.astype('int')
+df.index = pd.to_datetime(df.time, format='%Y/%m/%d %H:%M:%S')
+df.drop(columns=['time'], inplace = True)
+df['year'] = df.index.year.values
+df['month'] = df.index.month.values
+df['hour'] = df.index.hour.values
+df['week_day'] = df.index.weekday.values + 1
+
+df['age'] = df.year - df.birthyear
+
+df.drop(columns=['birthyear'], inplace=True)
+
+# Будем измерять длительность поездки в минутах
+df.tripduration /=60
+
+
+# В датасете имеются аномалии в виде аренд длительностью в тысячи минут:
+df.sort_values('tripduration', ascending=False).head(5)
+
+# Возможно, это случаи с забытыми велосипедами или несработавшими датчиками при закреплении велосипеда на станции и т.д.
+# Так или иначе, такие аренды считать поездками мы не будем
+
+
+# 99 процентов всех поездок длились меньше ~57 минут:
+
+
+np.percentile(df.tripduration, 99)
+
+# Для удобства работы с данными ограничимся арендой 60 минут. Этого более, чем достаточно
+df = df[df.tripduration < 60]
+
+
+# Save assembled data to file:
+
+with open('ALL.csv', mode='w', encoding='utf-8') as f_csv:
+    df.to_csv(f_csv)
+
+# Read assembled data from file:
+
+df = pd.read_csv('ALL.csv')
+
+df.index = pd.to_datetime(df.time, format='%Y/%m/%d %H:%M:%S')
+
+df.drop(columns='time', inplace=True)
+
+df.sort_index(inplace=True)
