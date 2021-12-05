5f07f4cba9459fa74232b987d246ff23bfe63a5a
RobertGaz <gazizov.robert@gmail.com>
2021-12-06 00:23:30 +0300

    fixes

--- src/data/dataset_processing.py
+++ src/data/dataset_processing.py
@@ -2,18 +2,10 @@ import numpy as np
 import pandas as pd
 import json
 import time
-import matplotlib.pyplot as plt
-import seaborn as sns
 import datetime
-import statsmodels.api as sm
-from scipy import stats
 
-data2016 = pd.read_csv('2016_.csv')
-data2017 = pd.read_csv('2017_.csv')
-data2018 = pd.read_csv('2018_.csv')
-data2019 = pd.read_csv('2019_.csv')
+df = pd.read_csv('../../data/processed/2019.csv')
 
-df = pd.concat([data2016, data2017, data2018, data2019], ignore_index=True, copy=False)
 df.drop_duplicates(subset='trip_id', inplace=True)
 df.drop(columns = ['usertype', 'start_time', 'end_time', 'trip_id', 'bikeid', 'from_station_id', 'from_station_name', 'to_station_id', 'to_station_name'], inplace=True)
 df.dropna(subset=['gender', 'birthyear'], inplace=True)
@@ -53,12 +45,12 @@ df = df[df.tripduration < 60]
 
 # Save assembled data to file:
 
-with open('ALL.csv', mode='w', encoding='utf-8') as f_csv:
+with open('../../data/processed/ALL.csv', mode='w', encoding='utf-8') as f_csv:
     df.to_csv(f_csv)
 
 # Read assembled data from file:
 
-df = pd.read_csv('ALL.csv')
+df = pd.read_csv('../../data/processed/ALL.csv')
 
 df.index = pd.to_datetime(df.time, format='%Y/%m/%d %H:%M:%S')
 
